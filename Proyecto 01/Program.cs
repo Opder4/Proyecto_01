670351020ce1daf284b11e0d5822578992d8e903
Emanuel Ramírez <er6348100@gmail.com>
2026-03-08 15:38:10 -0600

    Estructura General del Menú utilizando Do-While y Switch

--- Proyecto 01/Program.cs
+++ Proyecto 01/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine();
+﻿int opcion = -1;
+do
+{
+    Console.WriteLine("Menú");
+    Console.WriteLine("");
+    Console.WriteLine("1. Evaluar nuevo contendio");
+    Console.WriteLine("2. Mostrar reglas del sistema");
+    Console.WriteLine("3. Mostrar estadísticas de la sesión");
+    Console.WriteLine("4. Reiniciar estadísticas");
+    Console.WriteLine("5. Salir");
+    Console.WriteLine("");
+    opcion =int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    switch (opcion)
+    {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+
+        case 4:
+
+            break;
+        case 5:
+            Console.WriteLine("Saliendo del programa...");
+            Console.WriteLine("");
+            break;
+        default:
+            Console.WriteLine("Opción no válida");
+            Console.WriteLine("");
+            Console.WriteLine("Elija una opción que sea válida");
+            break;
+    }
+} while (opcion != 5);
+Console.WriteLine("Programa finalizado");
