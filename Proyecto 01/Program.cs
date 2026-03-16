22494a44914682e53f7881234f5db5bb69e99fae
Emanuel Ramírez <er6348100@gmail.com>
2026-03-15 20:36:40 -0600

    Se corrigio la Validación de Datos, debido a que la estructura anterior no funcionaba como debía

--- Proyecto 01/Program.cs
+++ Proyecto 01/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System.Xml;
-int tipocontenido = 0;
+int tipo = 0;
 int opcion = -1;
-double duracioncontendio = 0;
-int clasificacioncontenido = 0;
-int horaprogramacion = 0;
-int nivelproduccion = 0;
+double duracion = 0;
+int clasificacion = 0;
+int hora = 0;
+int produccion = 0;
+
 do
 {
     Console.WriteLine("Menú");
@@ -21,84 +22,10 @@ do
     switch (opcion)
     {
         case 1:
-            Console.WriteLine("Seleccione el tipo de contenido a envíar");
+            ValidacionDeDatos();
             Console.WriteLine();
-            Console.WriteLine("1. Película");
-            Console.WriteLine("2. Serie");
-            Console.WriteLine("3. Documental");
-            Console.WriteLine("4. Evento en vivo");
-            Console.WriteLine("");
-            tipocontenido=int.Parse(Console.ReadLine());
-            Console.WriteLine("");
-            Console.Clear();
-            //Aquí empieza la validación de los datos para proseguir con la evaluación
-            while (tipocontenido>=1 && tipocontenido<=4)
-            {
-                Console.WriteLine("Ingrese la duración del contendio (minutos): ");
-                Console.WriteLine("");
-                duracioncontendio=double.Parse(Console.ReadLine());
-                Console.WriteLine("");
-                Console.Clear();
-                while (duracioncontendio>0)
-                {
-                    Console.WriteLine("Seleccione la clasificación del contenido");
-                    Console.WriteLine("");
-                    Console.WriteLine("1. Para todo público");
-                    Console.WriteLine("2. Contenido +13");
-                    Console.WriteLine("3. Contenido +18");
-                    Console.WriteLine("");
-                    clasificacioncontenido=int.Parse(Console.ReadLine());
-                    Console.WriteLine("");
-                    Console.Clear();
-                    while (clasificacioncontenido>=1 && clasificacioncontenido<=3)
-                    {
-                        Console.WriteLine("Ingrese la hora de programación del contenido (0-23)");
-                        Console.WriteLine("");
-                        horaprogramacion=int.Parse(Console.ReadLine());
-                        Console.WriteLine("");
-                        Console.Clear();
-                        while (horaprogramacion>=0 && horaprogramacion<=23)
-                        {
-                            Console.WriteLine("Seleccione el nivel de producción del contenido");
-                            Console.WriteLine("");
-                            Console.WriteLine("1. Bajo");
-                            Console.WriteLine("2. Medio");
-                            Console.WriteLine("3. Alto");
-                            Console.WriteLine("");
-                            nivelproduccion=int.Parse(Console.ReadLine());
-                            Console.WriteLine("");
-                            Console.Clear();
-                            while (nivelproduccion>=1 && nivelproduccion<=3)
-                            {
-                                // Aquí empieza la validación técnica, la clasificación del tiempo, y la desición final
-                            }
-                            Console.WriteLine("Opción del nivel de producción no válida");
-                            Console.WriteLine("Presione ENTER");
-                            Console.WriteLine("");
-                            Console.ReadLine();
-                            Console.Clear();
-                        }
-                        Console.WriteLine("Hora de programación no válida");
-                        Console.WriteLine("Presione ENTER");
-                        Console.WriteLine("");
-                        Console.ReadLine();
-                        Console.Clear();
-                    }
-                    Console.WriteLine("Opción de clasificación no válida");
-                    Console.WriteLine("Presione ENTER");
-                    Console.WriteLine("");
-                    Console.ReadLine();
-                    Console.Clear();
-                }
-                Console.WriteLine("Duración de contenido no válido");
-                Console.WriteLine("Presione ENTER");
-                Console.WriteLine("");
-                Console.ReadLine();
-                Console.Clear();
-            }
-            Console.WriteLine("Opción de contenido no válida");
-            Console.WriteLine("Presione ENTER");
-            Console.WriteLine("");
+            Console.WriteLine("Presione ENTER para volver al menú");
+            Console.WriteLine();
             Console.ReadLine();
             Console.Clear();
             break;
@@ -110,12 +37,15 @@ do
             break;
 
         case 4:
-            duracioncontendio = 0;
-            clasificacioncontenido = 0;
-            horaprogramacion = 0;
-            nivelproduccion = 0;
-            tipocontenido = 0;
+            duracion = 0;
+            clasificacion = 0;
+            hora = 0;
+            produccion = 0;
+            tipo = 0;
             Console.WriteLine("Estadísticas reiniciadas");
+            Console.WriteLine();
+            Console.WriteLine("Presione Enter");
+            Console.WriteLine("");
             Console.ReadLine();
             Console.Clear();
             break;
@@ -127,9 +57,114 @@ do
             Console.WriteLine("Opción no válida");
             Console.WriteLine("");
             Console.WriteLine("Elija una opción que sea válida");
+            Console.WriteLine("");
+            Console.WriteLine("Presione Enter para continuar");
             Console.ReadLine();
             Console.Clear();
             break;
     }
 } while (opcion != 5);
 Console.WriteLine("Programa finalizado");
+
+void ValidacionDeDatos()
+{
+    Console.WriteLine("Seleccione el Tipo de Contenido a enviar");
+    Console.WriteLine("");
+    Console.WriteLine("1. Pelicula");
+    Console.WriteLine("2. Serie");
+    Console.WriteLine("3. Documental");
+    Console.WriteLine("4. En vivo");
+    Console.WriteLine("");
+    tipo = int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    while (tipo < 1 || tipo > 4)
+    {
+        Console.WriteLine("");
+        Console.Write("Error. Reingrese el tipo: ");
+        Console.WriteLine("");
+        tipo = int.Parse(Console.ReadLine());
+        Console.WriteLine("");
+    }
+
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine("");
+    Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine("");
+    Console.WriteLine("Ingrese la duración del contenido (minutos)");
+    Console.WriteLine("");
+    duracion = int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    while (duracion <= 0)
+    {
+        Console.WriteLine("");
+        Console.Write("Error. Reingrese duracion: ");
+        Console.WriteLine("");
+        duracion = int.Parse(Console.ReadLine());
+        Console.WriteLine("");
+    }
+
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine("");
+    Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine("Seleccione la clasificación del contenido");
+    Console.WriteLine("");
+    Console.WriteLine("1. Todo publico");
+    Console.WriteLine("2. +13");
+    Console.WriteLine("3. +18");
+    Console.WriteLine("");
+    clasificacion = int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    while (clasificacion < 1 || clasificacion > 3)
+    {
+        Console.WriteLine("");
+        Console.Write("Error. Reingrese clasificacion: ");
+        Console.WriteLine("");
+        clasificacion = int.Parse(Console.ReadLine());
+        Console.WriteLine("");
+    }
+
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine("");
+    Console.ReadLine();
+    Console.Clear();
+    Console.Write("");
+    Console.WriteLine("Ingrese la hora de transmisión del contenido (0-23)");
+    Console.WriteLine("");
+    hora = int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    while (hora < 0 || hora > 23)
+    {
+        Console.WriteLine("");
+        Console.Write("Error. Reingrese la hora: ");
+        Console.WriteLine("");
+        hora = int.Parse(Console.ReadLine());
+        Console.WriteLine("");
+    }
+
+    Console.WriteLine("Presione ENTER para continuar");
+    Console.WriteLine("");
+    Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine("Seleccione el nivel de producción del contenido");
+    Console.WriteLine("");
+    Console.WriteLine("1. Bajo");
+    Console.WriteLine("2. Medio");
+    Console.WriteLine("3. Alto");
+    Console.WriteLine("");
+    produccion = int.Parse(Console.ReadLine());
+    while (produccion < 1 || produccion > 3)
+    {
+        Console.WriteLine("");
+        Console.Write("Error. Reingrese el nivel: ");
+        Console.WriteLine("");
+        produccion = int.Parse(Console.ReadLine());
+        Console.WriteLine("");
+    }
+    Console.WriteLine("");
+    Console.WriteLine("Presione ENTER para continuar el proceso");
+    Console.WriteLine("");
+    Console.ReadLine();
+    Console.Clear();
+}
